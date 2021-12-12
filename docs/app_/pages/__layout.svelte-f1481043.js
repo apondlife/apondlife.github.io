962465559bbed266d34152affd6dff276bad1956
mutmedia <gustavo.ceci95@gmail.com>
2021-12-11 23:55:43 -0500

    build

---
+++ docs/app_/pages/__layout.svelte-f1481043.js
@@ -0,0 +1,11 @@
+import{S as A,i as j,s as x,e as g,t as _,c as $,a as v,g as h,d,b as o,f as D,D as l,E as w,F as S,k as q,j as C,n as G,m as H,o as P,G as R,H as T,I as V,x as I,u as O,v as z}from"../chunks/vendor-6fa2ee03.js";function B(c){let e,f,r,n,i,t,a,s,m,k,b;return{c(){e=g("footer"),f=_(`see about us and any upcoming events on\r
+  `),r=g("a"),n=_("instagram"),i=_(`\r
+  and\r
+  `),t=g("a"),a=_("tiktok"),s=_(`\r
+  and\r
+  `),m=g("a"),k=_("twitter"),b=_("."),this.h()},l(p){e=$(p,"FOOTER",{class:!0});var u=v(e);f=h(u,`see about us and any upcoming events on\r
+  `),r=$(u,"A",{href:!0,target:!0,rel:!0});var E=v(r);n=h(E,"instagram"),E.forEach(d),i=h(u,`\r
+  and\r
+  `),t=$(u,"A",{href:!0,target:!0,rel:!0});var y=v(t);a=h(y,"tiktok"),y.forEach(d),s=h(u,`\r
+  and\r
+  `),m=$(u,"A",{href:!0,target:!0,rel:!0});var F=v(m);k=h(F,"twitter"),F.forEach(d),b=h(u,"."),u.forEach(d),this.h()},h(){o(r,"href","https://instagram.com/gamesinthegrass"),o(r,"target","_blank"),o(r,"rel","noopener noreferrer"),o(t,"href","https://www.tiktok.com/@gamesinthegrass?"),o(t,"target","_blank"),o(t,"rel","noopener noreferrer"),o(m,"href","https://twitter.com/gamesinthegrass"),o(m,"target","_blank"),o(m,"rel","noopener noreferrer"),o(e,"class","Footer")},m(p,u){D(p,e,u),l(e,f),l(e,r),l(r,n),l(e,i),l(e,t),l(t,a),l(e,s),l(e,m),l(m,k),l(e,b)},p:w,i:w,o:w,d(p){p&&d(e)}}}class J extends A{constructor(e){super();j(this,e,null,B,x,{})}}function K(c){let e,f,r,n;const i=c[1].default,t=S(i,c,c[0],null);return r=new J({}),{c(){e=g("div"),t&&t.c(),f=q(),C(r.$$.fragment),this.h()},l(a){e=$(a,"DIV",{class:!0});var s=v(e);t&&t.l(s),f=G(s),H(r.$$.fragment,s),s.forEach(d),this.h()},h(){o(e,"class","Page")},m(a,s){D(a,e,s),t&&t.m(e,null),l(e,f),P(r,e,null),n=!0},p(a,[s]){t&&t.p&&(!n||s&1)&&R(t,i,a,a[0],n?V(i,a[0],s,null):T(a[0]),null)},i(a){n||(I(t,a),I(r.$$.fragment,a),n=!0)},o(a){O(t,a),O(r.$$.fragment,a),n=!1},d(a){a&&d(e),t&&t.d(a),z(r)}}}function L(c,e,f){let{$$slots:r={},$$scope:n}=e;return c.$$set=i=>{"$$scope"in i&&f(0,n=i.$$scope)},[n,r]}class N extends A{constructor(e){super();j(this,e,L,K,x,{})}}export{N as default};
