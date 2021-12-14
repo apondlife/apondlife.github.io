5841f44f45826c627918276bd413edc708cd8253
Ty Cobb <ty.cobb.m@gmail.com>
2021-12-14 00:30:22 -0500

    deploy

---
+++ docs/app_/pages/zines/rats/content/Cratique.svelte-1ea52b5e.js
@@ -0,0 +1,19 @@
+import{S as p,i as v,s as k,e as c,t as f,c as l,a as m,g as b,d as n,b as w,f as j,H as o,I as d}from"../../../../chunks/vendor-dd9689f8.js";/* empty css                                                            */function _(g){let t,e,a,r,h;return{c(){t=c("article"),e=c("h3"),a=c("b"),r=f("why i dislike rats as a theme for making games"),h=f(`
+    while they are interesting characters, and their cuteness or grossness is very appealing, rats have too much personality. its very
+    easy to make a game that has a rat in it, but it is hard to make it be about the rats as an object. its hard to interact with the
+    rat as a rat, not with the rat as a character. (the continuous use of cheese shows this a lot, we are making games with the caricature
+    of rats). additionally the games are not about the ratness of the rat, or in any way attempting to simulate/immerse/contemplate
+    the behaviours of rats. (sadly rats in bags didn't happen, but still, that is rats in bags as an object not the rats themselves).
+    using some design-speak, it is very hard to come up with the affordances of the rat (in this sense, its hard to objectify a rat).
+    one of my main interests in a theme like this, is its dissection (you can only dissect a dead rat, (or kill it in the process)).
+    i want a theme that is more dissectable, that we can explore many uses of our object, that we can make people think of the object
+    in ways they haven't before. to make videogames like still lives of objects. animals are not good objects, they are good characters.`),this.h()},l(s){t=l(s,"ARTICLE",{class:!0});var i=m(t);e=l(i,"H3",{});var u=m(e);a=l(u,"B",{});var y=m(a);r=b(y,"why i dislike rats as a theme for making games"),y.forEach(n),u.forEach(n),h=b(i,`
+    while they are interesting characters, and their cuteness or grossness is very appealing, rats have too much personality. its very
+    easy to make a game that has a rat in it, but it is hard to make it be about the rats as an object. its hard to interact with the
+    rat as a rat, not with the rat as a character. (the continuous use of cheese shows this a lot, we are making games with the caricature
+    of rats). additionally the games are not about the ratness of the rat, or in any way attempting to simulate/immerse/contemplate
+    the behaviours of rats. (sadly rats in bags didn't happen, but still, that is rats in bags as an object not the rats themselves).
+    using some design-speak, it is very hard to come up with the affordances of the rat (in this sense, its hard to objectify a rat).
+    one of my main interests in a theme like this, is its dissection (you can only dissect a dead rat, (or kill it in the process)).
+    i want a theme that is more dissectable, that we can explore many uses of our object, that we can make people think of the object
+    in ways they haven't before. to make videogames like still lives of objects. animals are not good objects, they are good characters.`),i.forEach(n),this.h()},h(){w(t,"class","Cratique svelte-vh6et7")},m(s,i){j(s,t,i),o(t,e),o(e,a),o(a,r),o(t,h)},p:d,i:d,o:d,d(s){s&&n(t)}}}class q extends p{constructor(t){super();v(this,t,null,_,k,{})}}export{q as default};
