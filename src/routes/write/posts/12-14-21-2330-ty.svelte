7639ff9ba6ab4e70d45caf8de790492015b08236
Ty Cobb <ty.cobb.m@gmail.com>
2021-12-15 00:10:38 -0500

    add some currently estranged posts

---
+++ src/routes/write/posts/12-14-21-2330-ty.svelte
@@ -0,0 +1,39 @@
+<svelte:head>
+  <title>blah</title>
+</svelte:head>
+
+<template>
+  <p>
+    i found myself w/ a mess of thoughts and feelings swirling around in my head this evening,
+    the sort of stuff i'd usually exorcise in a retro. everyone else is busy or unavailable tho,
+    so i beat metroid dread (ugh), grabbed an ice cream & six-pack of beer, and decided to write
+    (that these kind of thoughts often get left effectively undocumented on the retro board makes
+    me feel like we should find a way to easily serialize those conversations into a format that can
+    be left on the web instead. that others (or we) can revisit in the future. maybe a lightly edited /
+    auto-transcribed audio recording?).
+  </p>
+
+  <p>
+    we got some critical feedback about the our zine today that left me feeling pretty deflated. w/o
+    much motivation to continue working on the zine or any of the games in it. the feedback was principally
+    about layout decisions we'd made in response to some <em>other</em> feedback we've received. i
+    think i usually do a pretty good job of leaving feedback on the floor that i don't feel like serves
+    my own creative goals, and i think i kinda failed this time.
+  </p>
+
+  <p>
+    as a result, i'd spent a lot of effort this past weekend to try and pull some of the threads
+    together on the presentation-side of the zine, and it felt like work. and it <em>shouldn't</em> feel
+    like work. it should feel fun. that should've been the first sniff that something was off
+    (three "should"s in a row, no good). one nice thing about this particular piece of feedback is
+    that it's made me realize i was doing this work for purely external validation. it wasn't for me,
+    and that's not sustainable. what's the point of that? (a lot of "me"s too, but i don't want to
+    speak for anyone else).
+  </p>
+</template>
+
+<style>
+  p + p {
+    margin-top: 10px;
+  }
+</style>
